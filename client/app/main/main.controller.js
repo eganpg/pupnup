8b2ca035e018ad0724bd674afbda4b989ad3e262
eganpg <eganpg@gmail.com>
2015-04-03 13:40:58 -0700

    changing some stuff upp

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,21 +4,24 @@ angular.module('pupnupApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+      
+    // });
+    socket.syncUpdates('thing', $scope.awesomeThings);
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $scope.awesomeThings.push({ name: $scope.newThing });
+      // $http.post('/api/things', { name: $scope.newThing });
       $scope.newThing = '';
     };
 
     $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+      // $http.delete('/api/things/' + thing._id);
+      $scope.awesomeThings.pop(thing);
     };
 
     $scope.$on('$destroy', function () {
